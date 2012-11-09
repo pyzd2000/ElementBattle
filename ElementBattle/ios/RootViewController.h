e000287738b3f35d325cc78be9c4f079b24c1c31
frankpan <fankpan@gmail.com>
2012-11-09 11:02:56 +0800

    Initial Commit

---
+++ ElementBattle/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  ElementBattleAppController.h
+//  ElementBattle
+//
+//  Created by Frank Pan on 12-11-9.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
